62571623205f5afe36e40bc40aea8c75aa8339ce
Mohammad Salim.S.S <77904933+moh4b-z@users.noreply.github.com>
2024-12-06 16:56:08 -0300

    Atualizão do js para quando for visto no celular

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 var ativarScroll = document.querySelector('.ativicao')
+var headerul = document.querySelector('header ul')
 var linha = document.querySelector('.linha')
 var body = document.querySelector('body')
-var ul = document.querySelector('header ul')
-
 
 ativarScroll.addEventListener('click', function (event) {
+    if (window.innerWidth <= 735) {
+        linha.style.display = 'none'
+        headerul.style.display = 'none'
+    } else {
+        linha.style.display = 'flex'
+        headerul.style.display = 'flex'
+    }
+    
     body.style.overflowY = 'auto'
     body.style.height = 'auto'
     ativarScroll.style.display = 'none'
-    linha.style.display = 'flex'
-    ul.style.display = 'flex'
 
     window.scrollTo({
         top: linha.offsetTop, 
